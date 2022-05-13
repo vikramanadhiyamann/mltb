c2cfb7d82ae2e0d61b21776c3a6dcd28b5981d39
rootalt <105501815+rootalt@users.noreply.github.com>
2022-05-13 22:13:48 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,6 +1,6 @@
 if [ -z $TORRENT_TIMEOUT ]
 then
-   TORRENT_TIMEOUT=0
+   TORRENT_TIMEOUT=120
 fi
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt https://ngosang.github.io/trackerslist/trackers_all_http.txt https://newtrackon.com/api/all https://raw.githubusercontent.com/hezhijie0327/Trackerslist/main/trackerslist_tracker.txt | awk '$0' | tr '\n\n' ',')
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
@@ -9,4 +9,3 @@ aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
 --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
 --daemon=true --allow-overwrite=true --max-overall-download-limit=0 \
 --max-overall-upload-limit=1K --max-concurrent-downloads=4
-   
