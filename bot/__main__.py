fc27cecba876de622e9bb0bc57ccf7af78ff5d12
rootalt <105501815+rootalt@users.noreply.github.com>
2022-05-14 09:28:56 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -48,14 +48,14 @@ def stats(update, context):
     mem_u = get_readable_file_size(memory.used)
     stats = f'<b>Bot Uptime:</b> {currentTime}\n'\
             f'<b>Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
+            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\━━━━━━━━━━━━━━━━━━━━n'\
             f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
+            f'<b>Download:</b> {recv}\n━━━━━━━━━━━━━━━━━━━━\n'\
             f'<b>CPU:</b> {cpuUsage}% | '\
             f'<b>RAM:</b> {mem_p}% | '\
-            f'<b>DISK:</b> {disk}%\n\n'\
+            f'<b>DISK:</b> {disk}%\n━━━━━━━━━━━━━━━━━━━━\n'\
             f'<b>Physical Cores:</b> {p_core} | '\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
+            f'<b>Total Cores:</b> {t_core}\n━━━━━━━━━━━━━━━━━━━━\n'\
             f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
             f'<b>Memory Total:</b> {mem_t}\n'\
             f'<b>Memory Free:</b> {mem_a} | '\
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("secret repo heroku", "https://te.legra.ph/file/2c7b85d6b2d4d411d9088.jpg")
+    buttons.buildbutton("join to use bot", "https://t.me/+FybEXzvlVJk0ZDE1")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
@@ -214,7 +214,7 @@ def main():
                              else:
                                  bot.sendMessage(cid, msg, 'HTML')
                              msg = ''
-                if 'Restarted successfully!' in msg and cid == chat_id:
+                if '━━━━━━━━━━━━━━━━━━\n\nRestarted successfully\n\n━━━━━━━━━━━━━━━━━━' in msg and cid == chat_id:
                      bot.editMessageText(msg, chat_id, msg_id, parse_mode='HTMl')
                      osremove(".restartmsg")
                 else:
@@ -223,7 +223,7 @@ def main():
     if ospath.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
-        bot.edit_message_text("Restarted successfully!", chat_id, msg_id)
+        bot.edit_message_text("━━━━━━━━━━━━━━━━━━\n\nRestarted successfully\n\n━━━━━━━━━━━━━━━━━━", chat_id, msg_id)
         osremove(".restartmsg")
 
     start_handler = CommandHandler(BotCommands.StartCommand, start, run_async=True)
